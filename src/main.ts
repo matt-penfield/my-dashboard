24bf2d6667543d959fa1a92f0f7cabd07044b941
Matt Penfield <matt.penfield@slalom.com>
2026-06-29 10:33:43 -0700

    Add Vuetify and Material Design Icons to project dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
 import './assets/main.css'
+
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+  theme: {
+    defaultTheme: 'light',
+  },
+})
 
 const app = createApp(App)
 app.use(router)
+app.use(vuetify)
 app.mount('#app')
